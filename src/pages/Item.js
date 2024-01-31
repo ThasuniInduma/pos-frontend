da0e911205a310a2a92e3818980de4306b366397
Thasuni Induma <thasuniinduma@gmail.com>
2024-01-31 01:27:40 -0800

    Item complete

--- src/pages/Item.js
+++ src/pages/Item.js
@@ -2,6 +2,8 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import './Item.scss';
+import { FaEdit } from "react-icons/fa";
+import { MdDelete } from "react-icons/md";
 
 const Item = () => {
 
@@ -11,6 +13,9 @@ const Item = () => {
     const[price, setPrice] = useState(null);
     const[qty, setQty] = useState(0);
     const[categoryId, setCategoryId] = useState(null);
+    const[isEdit, setIsEdit] = useState(null);
+    const[editItemId, setEditItemId] = useState(null);
+    const[isOpen, setIsOpen] = useState(false);
     
     useEffect(() => {
         getItems();
@@ -44,10 +49,6 @@ const Item = () => {
         
     }
 
-  const handleLogout = () => {
-        localStorage.removeItem("token");
-        navigate("/login");
-    }
 
     const handleName = (event) => {
         setName(event.target.value);
@@ -65,9 +66,20 @@ const Item = () => {
         setCategoryId(event.target.value);
     }
 
+    const handleEdit = (itemId) => {
+        const selectedItem = items.find(item => item.id == itemId);
+        if(selectedItem){
+            setIsEdit(true);
+            setEditItemId(itemId);
+            setName(selectedItem.name);
+            setPrice(selectedItem.price);
+            setQty(selectedItem.qty);
+            setCategoryId(selectedItem.categoryId);
+        }
+    }
 
     const handleSubmit = async (event) => {
-        event.preventDefalt();
+        event.preventDefault();
         const data = {
             "name": name,
             "price": price,
@@ -75,32 +87,62 @@ const Item = () => {
             "categoryId": categoryId
         }
 
-        fetch("http://localhost:8080/items", {
-            method: 'POST',
-            headers:{
-                'Accept':'application/json',
-                'Content-Type':'application/json'
-            },
-            body:JSON.stringify(data)
-        }).then((response) => {
-            return response.json();
-        }).then((data) => {
-            setItems([...items, data]);
-            setName(null);
-            setPrice(null);
-            setQty(null);
-            setCategoryId(null);
-            console.log(items)
-        }).catch((error) => {
-            console.log(error);
-        })
+        
+        if(isEdit){
+            try {
+                await axios.put(`http://localhost:8080/items/${editItemId}`, data);
+                const updatedItems = items.map(item => (item.id === editItemId ? {...item, ...data} : item));
+                setItems(updatedItems);
+                setIsEdit(false);
+                setEditItemId(null);
+                setName("");
+                setPrice("");
+                setQty(0);
+                setCategoryId(null);
+                console.log("Item updated successfully");
+            } catch (error) {
+                console.error("Error updting item:", error);
+            }
+        } else {
+            try {
+                const response = await axios.post("http://localhost:8080/items", data, {
+                    headers:{
+                        'Accept':'application/json',
+                        'Content-Type':'application/json'
+                    }
+                });
+                setItems(prevItems => [...prevItems, response.data]);
+                setName("");
+                setPrice("");
+                setQty(0);
+                setCategoryId(null);
+                console.log("Item created successfully");
+
+            } catch (error) {
+                console.error("Error creating item", error);
+            }
+        }
 
     }
+
+
+    const handleDelete = async (itemId) => {
+        try {
+            await axios.delete(`http://localhost:8080/items/${itemId}`);
+            const updatedItems = items.filter(item => item.id !== itemId);
+            setItems(updatedItems);
+            console.log("Item deleted successfully");
+        } catch (error) {
+            console.error("Error deleting item:",error);
+        }
+    }
+
+    
     
     return (
             <div className="item-container">
                 <div className="form-container">
-                    <h1>Item</h1>
+                    <h1 className="cln">Item</h1>
                     <form className="form-item" onSubmit={handleSubmit}>
                         <div className="form-group mb-3">
                             <label className="form-label">Description</label>
@@ -126,7 +168,7 @@ const Item = () => {
                             </select>
                         </div>
                         
-                        <button type="submit" className="btn btn-secondary">Submit</button>
+                        <button type="submit" className="btn btn-secondary">{isEdit ? "Update" : "Submit"}</button>
                     </form>
                 </div>
                 <div className="card2">
@@ -137,17 +179,21 @@ const Item = () => {
                                 <th className="th2">Description</th>
                                 <th className="th3">Qty</th>
                                 <th className="th4">Unit Price</th>
+                                <th className="ha">Edit/Delete</th>
                             </tr>
                         </thead>
                         <tbody>
                             {items && items.map((item) =>(
-                            <tr key={item.id}>
-                                
+                                <tr key={item.id}>
                                     <td>{item.id}</td>
                                     <td>{item.name}</td>
                                     <td>{item.price}</td>
                                     <td>{item.qty}</td>
-                            </tr>
+                                    <td className="d1">
+                                        <div className="edit2" onClick={() => handleEdit(item.id)}><FaEdit /></div>
+                                        <div className="delete2" onClick={() => handleDelete(item.id)}><MdDelete /></div>
+                                    </td>
+                                </tr>
                             ))} 
                         </tbody>
                     </table>
